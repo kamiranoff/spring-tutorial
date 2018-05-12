29083dfb356e9c5afe4f590d236cc04565e80754
Kevin Amiranoff <kamiranoff@gmail.com>
2018-05-12 20:28:10 +0100

    adds default constructor for thymeleaf, create controllers and thymeleaf templates

---
+++ src/main/java/com/nemeantalestudios/spring5webapp/controllers/BookController.java
@@ -0,0 +1,26 @@
+package com.nemeantalestudios.spring5webapp.controllers;
+
+import com.nemeantalestudios.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author kevin
+ */
+
+@Controller
+public class BookController {
+
+    private BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "books";
+    }
+}
