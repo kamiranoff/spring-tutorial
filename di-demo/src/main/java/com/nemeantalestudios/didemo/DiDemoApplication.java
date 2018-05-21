edad2a43763b71f024d175b6edd301c4dafe9423
Kevin Amiranoff <kamiranoff@gmail.com>
2018-05-22 00:20:58 +0100

    adds external property source

--- di-demo/src/main/java/com/nemeantalestudios/didemo/DiDemoApplication.java
+++ di-demo/src/main/java/com/nemeantalestudios/didemo/DiDemoApplication.java
@@ -4,6 +4,7 @@ import com.nemeantalestudios.didemo.controllers.ConstructorInjectedController;
 import com.nemeantalestudios.didemo.controllers.MyController;
 import com.nemeantalestudios.didemo.controllers.PropertyInjectedController;
 import com.nemeantalestudios.didemo.controllers.SetterInjectedController;
+import com.nemeantalestudios.didemo.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -20,5 +21,11 @@ public class DiDemoApplication {
         System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
         System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
         System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+
+        FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+
+        System.out.println(fakeDataSource.getUser());
+        System.out.println(fakeDataSource.getPassword());
+        System.out.println(fakeDataSource.getUrl());
     }
 }
---
+++ di-demo/src/main/java/com/nemeantalestudios/didemo/config/PropertyConfig.java
@@ -0,0 +1,43 @@
+package com.nemeantalestudios.didemo.config;
+
+import com.nemeantalestudios.didemo.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+/**
+ * @author kevin
+ */
+
+@Configuration
+@PropertySource("classpath:datasource.properties")
+public class PropertyConfig {
+
+
+    @Value("${nemeantalestudios.user}")
+    String user;
+
+    @Value("${nemeantalestudios.password}")
+    String password;
+
+    @Value("${nemeantalestudios.dburl}")
+    String url;
+
+    @Bean
+    public FakeDataSource fakeDataSource() {
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUser(user);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setUrl(url);
+
+        return fakeDataSource;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() {
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        return propertySourcesPlaceholderConfigurer;
+    }
+}
