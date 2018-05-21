83616cf17ae364f0ff0e44d7c5718e154e096a66
Kevin Amiranoff <kamiranoff@gmail.com>
2018-05-21 21:17:03 +0100

    removed unsued annotation

--- jokeapp/src/main/java/com/nemeantalestudios/joke/controllers/JokeController.java
+++ jokeapp/src/main/java/com/nemeantalestudios/joke/controllers/JokeController.java
@@ -1,7 +1,6 @@
 package com.nemeantalestudios.joke.controllers;
 
 import com.nemeantalestudios.joke.services.JokeService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +14,6 @@ public class JokeController {
 
     private JokeService jokeService;
 
-    @Autowired
     public JokeController(JokeService jokeService) {
         this.jokeService = jokeService;
     }
