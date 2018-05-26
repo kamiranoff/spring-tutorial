17b40f793dedb9e068c67a1f7f3e985d7d19bc6e
Kevin Amiranoff <kevin.amiranoff@zopa.com>
2018-05-26 20:08:05 +0100

    fix Debug logs not appearing and prevent Error with Transactional Annotation

--- recipe/src/main/java/com/nemeantalestudios/recipe/bootstrap/RecipeBootstrap.java
+++ recipe/src/main/java/com/nemeantalestudios/recipe/bootstrap/RecipeBootstrap.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.*;
@@ -29,7 +30,9 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
         this.unitOfMeasureRepository = unitOfMeasureRepository;
     }
 
+
     @Override
+    @Transactional
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
         recipeRepository.saveAll(getRecipes());
     }
@@ -111,7 +114,7 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
 
         recipes.add(guacamoleRecipe);
 
-        log.debug("Returning Recipes",recipes);
+        log.debug("Returning Recipes");
 
         return recipes;
 
